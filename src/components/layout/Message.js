9c81d391ed6f004b8491e05294878ad4af1ad98b
Evandro Florscuk <evandro_ff@me.com>
2023-05-16 02:28:47 -0300

    Project updated => Message component created and implemented

--- src/components/layout/Message.js
+++ src/components/layout/Message.js
@@ -1,8 +1,33 @@
+import { useState, useEffect } from 'react'
+
 import styles from "./Message.module.css"
 
 const Message = ({type, msg}) => {
+    const [visible, setVisible] = useState(false)
+
+    useEffect(() => {
+        if(!msg) {
+            setVisible(false)
+            return
+        }
+
+        setVisible(true)
+
+        const timer = setTimeout(() => {
+            setVisible(false)
+        }, 3000)
+
+        return () => clearTimeout(timer)
+
+    }, [msg])
+
+
     return (
-        <p>Message</p>
+        <>
+            {visible && (
+                <div className={`${styles.message} ${styles[type]}`}>{msg}</div>
+            )}
+        </>
     )
 }
 
