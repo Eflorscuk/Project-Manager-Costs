4df3c0f18e6196faf0d89a3734cc1e07b2557bd9
Evandro Florscuk <evandro_ff@me.com>
2023-05-17 02:44:25 -0300

    Project Updated => Dashboard created

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,6 +1,9 @@
 import { useLocation } from "react-router-dom"
 
 import Message from "../layout/Message"
+import styles from "./Projects.module.css"
+import Container from "../layout/Container"
+import LinkButton from "../layout/LinkButton"
 
 const Projects = () => {
     const location = useLocation()
@@ -10,9 +13,15 @@ const Projects = () => {
     }
     
     return (
-        <div>
-            <h1>My Projects</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>My Projects</h1>
+                <LinkButton to="/newproject" text="Create a new project"/>
+            </div>
             {message && <Message type="success" msg={message}/>}
+            <Container customClass="start">
+                <p>Project...</p>
+            </Container>
         </div>
     )
 }
